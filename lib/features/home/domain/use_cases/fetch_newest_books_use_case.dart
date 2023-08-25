ad69cc2c23992726a408797dda89d5c46a59276f
AbdAlrhman Hassan <aalrhman675@gmail.com>
2023-08-26 01:08:42 +0300

    42. Fetch newest books use case

---
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app_advanced_course/core/use_cases/no_parameter_use_case.dart';
+import 'package:bookly_app_advanced_course/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app_advanced_course/features/home/domain/repo/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>> {
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase({required this.homeRepo});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() async {
+    return await homeRepo.fetchNewsetBooks();
+  }
+}
