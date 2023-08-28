bb24a8cdafea7110b89184653334c61f587bb382
AbdAlrhman Hassan <aalrhman675@gmail.com>
2023-08-28 21:22:29 +0300

    59. Fetch featured books home repo impl

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,20 +1,43 @@
 import 'package:bookly_app_advanced_course/core/errors/failure.dart';
+import 'package:bookly_app_advanced_course/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bookly_app_advanced_course/features/home/data/data_sources/home_remote_data_sources.dart';
 import 'package:bookly_app_advanced_course/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app_advanced_course/features/home/domain/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl extends HomeRepo
-{
+class HomeRepoImpl extends HomeRepo {
+  final HomeLocalDataSource homeLocalDataSource;
+  final HomeRemoteDataSource homeRemoteDataSource;
+
+  HomeRepoImpl(
+      {required this.homeLocalDataSource, required this.homeRemoteDataSource});
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      var booksList = homeLocalDataSource.fetchFeaturedBooks();
+      if (booksList.isNotEmpty) {
+        return right(booksList);
+      } else {
+        var books = await homeRemoteDataSource.fetchFeaturedBooks();
+        return (right(books));
+      }
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewsetBooks() {
-    // TODO: implement fetchNewsetBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchNewsetBooks() async {
+    try {
+      var booksList = homeLocalDataSource.fetchNewsetBooks();
+      if (booksList.isNotEmpty) {
+        return right(booksList);
+      } else {
+        var books = await homeRemoteDataSource.fetchNewsetBooks();
+        return (right(books));
+      }
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
   }
-  
 }
